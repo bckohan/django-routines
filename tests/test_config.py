8178545c3a2550860b68f787219b972b25279df3
Brian Kohan <bckohan@gmail.com>
2024-06-04 17:36:49 -0700

    tests for coverage

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,19 +1,7 @@
-from io import StringIO
-from importlib.util import find_spec
-import pytest
-
-from django.core.management import BaseCommand, call_command, CommandError
 from django_typer import get_command
 from django.test import TestCase, override_settings
-from django.conf import settings
-from django_routines import ROUTINE_SETTING
 from django.core.exceptions import ImproperlyConfigured
-import re
-
-from tests.django_routines_tests.management.commands.track import (
-    invoked,
-    passed_options,
-)
+from django_routines import get_routine
 
 
 class TestBadConfig(TestCase):
@@ -48,3 +36,36 @@ class TestBadConfig(TestCase):
     def test_malformed_command(self):
         with self.assertRaises(ImproperlyConfigured):
             get_command("routine")
+
+    @override_settings(
+        DJANGO_ROUTINES={
+            "good": {
+                "commands": [
+                    {"command": ("track", "0")},
+                    {"command": ("track", "1")},
+                ],
+                "help_text": "A good routine",
+                "name": "good",
+            },
+        }
+    )
+    def test_get_missing_routine(self):
+        self.assertEqual(get_routine("good").name, "good")
+        with self.assertRaises(KeyError):
+            get_routine("dne")
+
+    @override_settings(
+        DJANGO_ROUTINES={
+            "bad_command": {
+                "commands": [
+                    {"command": ("track", "0")},
+                    {"comand": ("track", "1")},
+                ],
+                "help_text": "Bad command test routine",
+                "name": "bad_command",
+            },
+        }
+    )
+    def test_get_malformed_routine(self):
+        with self.assertRaises(ImproperlyConfigured):
+            get_routine("bad_command")
